04de114bda6ac8e89f6d3aecc5b926ef44e6a729
Daan Gerits <daan@bigboards.io>
2015-06-22 09:25:56 +0200

    added multiple architectures.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch i386 -c $1 --access-key-id=$2 --secret-access-key=$3 target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $1 --access-key-id=$2 --secret-access-key=$3 target/*.deb
