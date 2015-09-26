31c20c7574a99ca3195b1e6b58a485fbf077e51d
Daan Gerits <daan@bigboards.io>
2015-09-26 09:48:55 +0200

    added the new client

--- client/app/core.js
+++ client/app/core.js
@@ -14,7 +14,8 @@ angular.module('hive.core', [])
         { codename: 'genesis', version: '0.5.0'},
         { codename: 'feniks', version: '1.0.0'},
         { codename: 'ember', version: '1.1.0'},
-        { codename: 'gemini', version: '1.2.0'}
+        { codename: 'gemini', version: '1.2.0'},
+        { codename: 'v1.3', version: '1.3.0'}
     ])
     .constant('Architectures', [
         'all',
