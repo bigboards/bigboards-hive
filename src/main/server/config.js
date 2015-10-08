1aedfde38449af4fb6ea7151e054fae05a4e5fae
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-10-08 18:10:25 +0200

    Properly configure a secured connection to ES on found.io

--- src/main/server/config.js
+++ src/main/server/config.js
@@ -17,8 +17,12 @@ module.exports = {
             port: 3020,
             content: 'src/main/client',
             elasticsearch: {
-                host: 'https://469c3fcba4d983641ddd30557d30b356.us-east-1.aws.found.io:9243',
-                auth: 'searchonly:23c42x4bkegoghmw8h',
+                host: [{
+                    protocol: 'https',
+                    host: '469c3fcba4d983641ddd30557d30b356.us-east-1.aws.found.io',
+                    port: '9243',
+                    auth: 'readwrite:tcsss4frsejb429qk2'
+                }],
                 log: 'debug',
                 apiVersion: '1.5'
             },
