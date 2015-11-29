8a388948137594dc70f8bdd4a5330adf42dddc51
Daan Gerits <daan@bigboards.io>
2015-11-29 11:51:48 +0100

    tweaked the device cards

--- client/app/devices/module.js
+++ client/app/devices/module.js
@@ -27,6 +27,29 @@ deviceModule.directive('bbDeviceCard', [function() {
         },
         templateUrl: 'app/devices/cards/device-card.tmpl.html',
         controller: ['$scope', '$location', 'DeviceResource', function($scope, $location, DeviceResource) {
+            $scope.totalMemory = 0;
+            $scope.totalCores = 0;
+            $scope.totalStorage = 0;
+
+            $scope.$watch('device', function(value) {
+                if (! value) return;
+
+                if (value.data.memory) $scope.totalMemory += parseInt(value.data.memory * 1024);
+                if (value.data.cpus) {
+                    if ( Object.prototype.toString.call( value.data.cpus ) === '[object Array]' ) {
+                        $scope.totalCores += value.data.cpus.length;
+                    } else {
+                        $scope.totalCores += 1;
+                    }
+                }
+                if (value.data.disks) {
+                    value.data.disks.forEach(function(disk) {
+                        if (disk.type != 'data') return;
+
+                        $scope.totalStorage += (disk.size * 1024);
+                    });
+                }
+            });
         }]
     };
 }]);
